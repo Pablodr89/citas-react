62928573879fd387dc628de034fc4df9e1e5ad25
Pablodr89 <padoro89@gmail.com>
2024-01-10 20:40:36 +0100

    editar, eliminar y useEffect

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,31 +1,40 @@
-import Paciente from "./Paciente"
+import Paciente from "./Paciente";
 
-function ListadoPacientes({pacientes}) {
-  
+function ListadoPacientes({ pacientes, setPaciente, eliminarPaciente }) {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-        <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-        <p className="text-xl mt-5 mb-10 text-center">
-            Administra tus {''}
-            <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
-        </p>
+      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {""}
+        <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
+      </p>
 
-        {pacientes && pacientes.length ? (
-          <>
-            {pacientes.map((paciente) => (
-              <Paciente paciente={paciente} key={paciente.id} />
-            ))}
-          </>
-        ) : (
-          <>
-            <h2 className="font-black text-3xl text-center mt-64">No hay pacientes</h2>
-            <p className="text-xl mt-5 mb-10 text-center">Comienza agregando pacientes {''}
-              <span className="text-indigo-600 font-bold ">y aparecerán en este lugar</span>
-            </p>
-          </>
-        ) }
+      {pacientes && pacientes.length ? (
+        <>
+          {pacientes.map((paciente) => (
+            <Paciente
+              paciente={paciente}
+              key={paciente.id}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center mt-64">
+            No hay pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold ">
+              y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default ListadoPacientes
+export default ListadoPacientes;
